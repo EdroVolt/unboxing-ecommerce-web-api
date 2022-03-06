139fd81de094f9f1029127eb735953c33a743b3f
Rana Wagdi <73128713+rana2020701@users.noreply.github.com>
2022-03-06 18:10:24 +0200

    EC-2-bd-feat-create-product-model

--- src/models/Product.model.ts
+++ src/models/Product.model.ts
@@ -1,66 +1,55 @@
-const mongoose=require("mongoose");
+import mongoose from "mongoose";
+
 
 const schema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  count: Number,
+  size_count: {
+    xs: Number,
+    s: Number,
+    md: Number,
+    l: Number,
+    xl: Number
+  },
+  ingredients: [String],
+  images: { type: String, required: true },
+  price: { type: Number, required: true },
+  discount: Number,
+  offer: Number,
+  reviews: [
+    {
+      comment: String,
+      rate: Number
+    }
+  ],
+  numOfReviews: Number
+});
 
-        "name": {type:String, required:true},
-        "description":{type:String, required:true},
-        "count": Number,
-        "size_count": {
-          "xs": Number,
-          "s": Number,
-          "md": Number,
-          "l": Number,
-          "xl": Number
-        },
-
-        "ingredients": [String],
-        "images": {type:String, required:true},
-        "price":  {type:Number, required:true},
-        "discount": Number,
-        "offer": Number,
-        "reviews": [
-          { 
-           
-            "comment": String,
-             "rate": Number
-          }
-        ],
-        "numOfReviews": Number,
-      
-
-})
-
-module.exports = mongoose.model("products",schema)
-
-
+export const ProductModel = mongoose.model('products', schema);  
 
 export interface Product {
+  name: string;
+  description: string;
+  count: number;
+  size_count: {
+    xs: number;
+    s: number;
+    md: number;
+    l: number;
+    xl: number;
+  };
 
-
-    "name": string,
-    "description":string,
-    "count": number,
-    "size_count": {
-      "xs": number,
-      "s": number,
-      "md": number,
-      "l": number,
-      "xl": number
-    },
-
-    "ingredients": [string],
-    "images":string,
-    "price":  number,
-    "discount": number,
-    "offer": number,
-    "reviews": [
-      { 
-       
-        "comment": string,
-         "rate": number
-      }
-    ],
-    "numOfReviews": number,
-  
-
+  ingredients: [string];
+  images: string;
+  price: number;
+  discount: number;
+  offer: number;
+  reviews: [
+    {
+      comment: string;
+      rate: number;
+    }
+  ];
+  numOfReviews: number;
 }

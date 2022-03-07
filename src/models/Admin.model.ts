9f2c0da7b4feadc9da5a797a41c780ff4806c727
yasmenMohammed1 <yasmen.abodabash@gmail.com>
2022-03-07 15:31:18 +0200

    bd-feat: create admin model

--- src/models/Admin.model.ts
+++ src/models/Admin.model.ts
@@ -3,20 +3,18 @@ import mongoose from 'mongoose';
 // creat Admin schema
 const schema = new mongoose.Schema({
   _id: mongoose.Types.ObjectId,
-
   name: { String, required: true },
   email: { String, required: true },
   password: { String, required: true },
-
   role: String
 });
 
+mongoose.model('admins', schema);
 // generate admin type as Admin interface
-export interface User {
+export interface Admin {
   _id: mongoose.Types.ObjectId;
-
-  name: string;
-  email: string;
-  password: string;
+  name: String;
+  email: String;
+  password: String;
   role: String;
 }

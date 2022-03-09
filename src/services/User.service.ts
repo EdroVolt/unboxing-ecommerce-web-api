f985908d0c9afdfbdfe700894040f68aaed83b9f
Ahmed Edrees <ahmededrees30@gmail.com>
2022-03-09 19:39:48 +0200

    bd-init: initialize UserService

---
+++ src/services/User.service.ts
@@ -0,0 +1,15 @@
+import UserRepo from '../repositories/User.repo';
+
+const userRepo = new UserRepo();
+
+export class UserService {
+  // TODO: findAll()
+
+  // TODO: findOne()
+
+  // TODO: createOne()
+
+  // TODO: updateOne()
+
+  // TODO: deleteOne()
+}

cfa34d98f52d2638175ba9e27caac526d7184c76
Ahmed Edrees <ahmededrees30@gmail.com>
2022-03-09 19:50:40 +0200

    bd-fix: change filename and some typos

---
+++ src/services/Category.service.ts
@@ -0,0 +1,15 @@
+import { CategoryRepo } from '../repositories/Category.repo';
+
+const categoryRepo = new CategoryRepo();
+
+export class CategoryService {
+  // TODO: findAll()
+
+  // TODO: findOne()
+
+  // TODO: createOne()
+
+  // TODO: updateOne()
+
+  // TODO: deleteOne()
+}

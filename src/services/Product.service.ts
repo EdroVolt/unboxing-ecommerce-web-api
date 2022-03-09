9f0457101fdf12818cedd19eae5c45e3eff8a8eb
Ahmed Edrees <ahmededrees30@gmail.com>
2022-03-09 19:53:52 +0200

    bd-init: initialize ProductService

---
+++ src/services/Product.service.ts
@@ -0,0 +1,15 @@
+import { ProductRepo } from '../repositories/Product.repo';
+
+const productRepo = new ProductRepo();
+
+export class ProductService {
+  // TODO: findAll()
+
+  // TODO: findOne()
+
+  // TODO: createOne()
+
+  // TODO: updateOne()
+
+  // TODO: deleteOne()
+}

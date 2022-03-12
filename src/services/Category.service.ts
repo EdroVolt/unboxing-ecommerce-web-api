7a06aa870bafb7e066f31ad23e13fe56baaa6a5e
Nesma Taha <nesmataha91@gmail.com>
2022-03-12 21:50:29 +0200

    bd-feat: create findAll() for CategoryService

--- src/services/Category.service.ts
+++ src/services/Category.service.ts
@@ -4,6 +4,14 @@ const categoryRepo = new CategoryRepo();
 
 export class CategoryService {
   // TODO: findAll()
+  async findAll() {
+    try {
+      const categoryDoc = await categoryRepo.findAll();
+      return categoryDoc
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   // TODO: findOne()
 

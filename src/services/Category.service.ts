4373be7f6e139d3c1fe3c8147ef3da7bef7d04be
menna refaat <menna.ref3t@gmail.com>
2022-03-12 20:28:42 +0200

    bd-feat: create deleteOne() for CategoryService

--- src/services/Category.service.ts
+++ src/services/Category.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import { CategoryRepo } from '../repositories/Category.repo';
 
 const categoryRepo = new CategoryRepo();
@@ -12,4 +13,12 @@ export class CategoryService {
   // TODO: updateOne()
 
   // TODO: deleteOne()
+  async deletOne(_id: mongoose.Types.ObjectId | number) {
+    try {
+      const categoryDoc = await categoryRepo.deleteOne(_id)
+      return categoryDoc
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }

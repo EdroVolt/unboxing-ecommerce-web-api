baae668058f6b68bd15364f470fc863b78060808
Ahmed Edrees <ahmededrees30@gmail.com>
2022-03-14 01:21:47 +0200

    bd-feat: create createOne() for UserServicer

--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -1,3 +1,4 @@
+import { User } from 'models/Users.model';
 import { UserRepo } from '../repositories/User.repo';
 
 const userRepo = new UserRepo();
@@ -8,6 +9,14 @@ export class UserService {
   // TODO: findOne()
 
   // TODO: createOne()
+  async createOne(data: User) {
+    try {
+      const usersDoc = await userRepo.create(data);
+      return usersDoc;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   // TODO: updateOne()
 

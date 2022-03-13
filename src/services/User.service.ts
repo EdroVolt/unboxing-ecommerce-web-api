eed4e29677dc6b4f8b72b82cda9f77ba637f0c13
Ahmed Edrees <ahmededrees30@gmail.com>
2022-03-14 01:13:47 +0200

    bd-feat: create findAll() for UserService

--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -4,6 +4,14 @@ const userRepo = new UserRepo();
 
 export class UserService {
   // TODO: findAll()
+  async findAll(filter: Object = {}) {
+    try {
+      const usersDocs = await userRepo.findAll(filter);
+      return usersDocs;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   // TODO: findOne()
 

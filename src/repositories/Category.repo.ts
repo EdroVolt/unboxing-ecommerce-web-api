6e66333c256ecb9cadf9be60aaf7e9942c6fcd93
Ahmed Mohamed Abo Edrees <ahmededrees30@gmail.com>
2022-03-13 18:32:46 +0200

    Delete Category.repo.ts

--- src/repositories/Category.repo.ts
+++
@@ -1,7 +0,0 @@
-import { Category, CategoryModel } from '../models/Category.model';
-import { BaseRepo } from './Base.repo';
-
-export class CategoryRepo extends BaseRepo<Category> {
-    _collectionName: string = 'categories';
-    _model: Object = CategoryModel;
-}

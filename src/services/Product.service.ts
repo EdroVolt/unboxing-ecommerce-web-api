0bd67cddba28902903fff50becfaead6aefa1182
yasmenMohammed1 <yasmen.abodabash@gmail.com>
2022-03-13 19:36:19 +0200

    bd-feat: create findAll() for ProductService

--- src/services/Product.service.ts
+++ src/services/Product.service.ts
@@ -1,10 +1,20 @@
+import { Product } from 'models/Product.model';
+import mongoose from 'mongoose';
+
 import { ProductRepo } from '../repositories/Product.repo';
 
-const productRepo = new ProductRepo();
+const productRepo: ProductRepo = new ProductRepo();
 
 export class ProductService {
   // TODO: findAll()
-
+  async findAll() {
+    try {
+      const productDoc = await productRepo.findAll();
+      return productDoc;
+    } catch (error) {
+      console.error(error);
+    }
+  }
   // TODO: findOne()
 
   // TODO: createOne()
